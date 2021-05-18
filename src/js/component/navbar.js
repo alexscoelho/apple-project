bcd335c255b9dbd19be5e3d132a20e1a9a85b7f4
Alexson Coelho <coelho@Alexsons-MacBook-Pro.local>
2021-05-18 19:48:38 -0400

    responsive navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,25 +3,37 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
 			<div className="black-box-icon">
 				<Link to="/">
 					<i className="fab fa-apple fa-2x apple" />
 				</Link>
 			</div>
-			<div className="ml-auto">
-				<Link className="mr-5" to="/phone">
-					iphone
-				</Link>
-				<Link className="mr-5" to="/mackbook">
-					MackBook Pro
-				</Link>
-				<Link className="mr-5" to="/watch">
-					Watch
-				</Link>
-				<Link className="mr-5" to="/watch">
-					<div className="btn btn-primary">Notify me</div>
-				</Link>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNavAltMarkup"
+				aria-controls="navbarNavAltMarkup"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon" />
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div className="navbar-nav ml-auto">
+					<Link className="mr-5 nav-link " to="/phone">
+						iPhone
+					</Link>
+					<Link className="mr-5 nav-link " to="/mackbook">
+						MackBook Pro
+					</Link>
+					<Link className="mr-5 nav-link " to="/watch">
+						Watch
+					</Link>
+					<Link className="mr-5" to="/">
+						<div className="btn btn-primary">Notify me</div>
+					</Link>
+				</div>
 			</div>
 		</nav>
 	);
