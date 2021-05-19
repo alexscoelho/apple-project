92bf7946c00a2f777937b1da3bd8afef77caf1ce
Alexson Coelho <coelho@Alexsons-MacBook-Pro.local>
2021-05-18 21:43:49 -0400

    watch view

--- src/js/views/watch.js
+++ src/js/views/watch.js
@@ -1,8 +1,51 @@
 import React from "react";
-import "../../styles/home.scss";
+import "../../styles/watch.scss";
+import blackWatch from "../../img/Black Watch-1.png";
+import { IoWatch } from "react-icons/io5";
 
 export const Watch = () => (
-	<div className="text-center mt-5">
-		<h1>Watch</h1>
-	</div>
+	<>
+		<div className="jumbotron jumbotron-fluid">
+			<div className="container">
+				<div className="row">
+					<div className="col-5">
+						<p className="text-danger">Apple Watch</p>
+						<h1 className="display-4 ">Change starts within.</h1>
+						<p className="lead ">
+							This is a modified jumbotron that occupies the entire horizontal space of its parent.
+						</p>
+						<p className="text-danger">Starts shipping MM-DD-YYY</p>
+					</div>
+					<div className="col-5">
+						<img src={blackWatch} className="img-fluid" alt={blackWatch} />
+					</div>
+					<div className="col-2 d-flex align-items-center">
+						<ul className="right-side-icons">
+							<li>
+								<i className="fas fa-mobile-alt" />
+							</li>
+							<li>
+								<i className="fas fa-laptop" />
+							</li>
+							<li>
+								<IoWatch />
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div className="container">
+			<div className="row">
+				<div className="col-6">
+					<h1>From $699</h1>
+					<p className="text-danger">Buy Now </p>
+				</div>
+				<div className="col-6 d-flex justify-content-center">
+					<i className="far fa-circle mr-5" />
+					<i className="fas fa-circle" />
+				</div>
+			</div>
+		</div>
+	</>
 );
