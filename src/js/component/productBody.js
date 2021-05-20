33965345887299c8d7cb1cc66704677136d7490b
Alexson Coelho <coelho@Alexsons-MacBook-Pro.local>
2021-05-19 22:00:11 -0400

    modifications to product body

--- src/js/component/productBody.js
+++ src/js/component/productBody.js
@@ -5,45 +5,42 @@ import PropTypes from "prop-types";
 
 export const ProductBody = ({ product, title, description, other, productImage }) => {
 	return (
-		<div className="jumbotron jumbotron-fluid">
-			{/* <div className="hermano"> */}
-			<div className="container">
-				<div className="row">
-					<div className="col-md-5">
-						<p className="text-danger">{product}</p>
-						<h1 className="display-4 ">{title}</h1>
-						<p className="lead ">{description}</p>
-						<p className="text-danger">{other}</p>
-					</div>
-					<div className="col-md-5">
-						<img src={productImage} className="img-fluid" alt={productImage} />
-					</div>
-					<div className="col-sm d-flex align-items-center">
-						<ul className="right-side-icons">
-							<li>
-								<Link to="/phone">
-									<i className="fas fa-mobile-alt" />
-								</Link>
-							</li>
-							<li>
-								<Link to="/mackbook">
-									<i className="fas fa-laptop" />
-								</Link>
-							</li>
-							<li>
-								<Link to="/watch">
-									<IoWatch />
-								</Link>
-							</li>
-						</ul>
+		<div className="jumbotron jumbotron-fluid product-body">
+			<div className="content-wrapper">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-6">
+							<p className="text-danger">{product}</p>
+							<h1 className="display-4 ">{title}</h1>
+							<p className="lead ">{description}</p>
+							<p className="text-danger">{other}</p>
+						</div>
+						<div className="col-sm-6 col-md-6 d-flex align-items-center right-side-icons">
+							<ul className="right-side-icons">
+								<li>
+									<Link to="/phone">
+										<i className="fas fa-mobile-alt" />
+									</Link>
+								</li>
+								<li>
+									<Link to="/mackbook">
+										<i className="fas fa-laptop" />
+									</Link>
+								</li>
+								<li>
+									<Link to="/watch">
+										<IoWatch />
+									</Link>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
-			</div>
-			{/* <div className="col-md-5">
+				<div className="col-md-5 product-image">
 					<img src={productImage} className="img-fluid" alt={productImage} />
-				</div> */}
+				</div>
+			</div>
 		</div>
-		// </div>
 	);
 };
 
--- src/js/views/phone.js
+++ src/js/views/phone.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { ProductBody } from "../component/productBody";
 import goldPhone from "../../img/apple-iphonexs-max-gold-back-2.png";
+import smallPhone1 from "../../img/apple-iphonexs-max-gold.png";
+import smallPhone2 from "../../img/apple-iphonexs-max-gold-back-3.png";
 
 export const Phone = () => (
 	<>
@@ -13,14 +15,42 @@ export const Phone = () => (
 		/>
 		<div className="container">
 			<div className="row">
-				<div className="col-6">
+				<div className="col-3">
 					<h1>From $699</h1>
 					<p className="text-danger">Buy Now </p>
 				</div>
-				<div className="col-6 d-flex justify-content-center">
-					<i className="far fa-circle mr-5" />
-					<i className="fas fa-circle" />
-				</div>
+				{/* <div className="col-6">
+					<div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+						<ol className="carousel-indicators">
+							<li data-target="#carouselExampleIndicators" data-slide-to="0" className="active" />
+							<li data-target="#carouselExampleIndicators" data-slide-to="1" />
+						</ol>
+						<div className="carousel-inner">
+							<div className="carousel-item active">
+								<img src={smallPhone1} className="d-block " alt={smallPhone1} />
+							</div>
+							<div className="carousel-item">
+								<img src={smallPhone2} className="d-block " alt="..." />
+							</div>
+						</div>
+						<a
+							className="carousel-control-prev"
+							href="#carouselExampleIndicators"
+							role="button"
+							data-slide="prev">
+							<span className="carousel-control-prev-icon" aria-hidden="true" />
+							<span className="sr-only">Previous</span>
+						</a>
+						<a
+							className="carousel-control-next"
+							href="#carouselExampleIndicators"
+							role="button"
+							data-slide="next">
+							<span className="carousel-control-next-icon" aria-hidden="true" />
+							<span className="sr-only">Next</span>
+						</a>
+					</div>
+				</div> */}
 			</div>
 		</div>
 	</>
